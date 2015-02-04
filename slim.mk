f3ddf8752c59fef4e8bd3435c658bf703ac8cf64
mydongistiny <jaysonedson@gmail.com>
2015-02-04 05:03:55 +0000

    Remove cm overlay.

--- slim.mk
+++ slim.mk
@@ -17,8 +17,6 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/motorola/shamu/aosp_shamu.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/motorola/shamu/overlay-cm
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := slim_shamu
 PRODUCT_BRAND := Google
